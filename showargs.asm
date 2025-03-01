0e0a7d73b6a525b251423ca189f53067c90ec254
B.Billion <buhebillion@gmail.com>
2025-03-01 08:37:23 +0200

    .bss section

--- showargs.asm
+++ showargs.asm
@@ -12,4 +12,17 @@ SECTION .data
 ERRMSG DB "Terminated with error.",10
 ERRLEN EQU $-ERRMSG
 
+SYS_WRITE_CALL_VAL EQU 1
+STDERR_FD          EQU 2
+SYS_READ_CALL_VAL  EQU 0
+STDIN_FD           EQU 0
+STDOUT_FD          EQU 1
+EXIT_SYSCALL       EQU 60
+OK_RET_VAL         EQU 0
+EOF_VAL						 EQU 0
+EOL                EQU 10
+
 SECTION .bss
+
+MAXARGS            EQU 15             ;Maximum # of args we support <16: 15 + Invocation text>
+ARGLENS:           RESQ MAXARGS       ;Table of argument lengths
